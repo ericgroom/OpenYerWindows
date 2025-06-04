644163985c794f71b72d08e292573565b4ce2928
Eric Groom <eric@gingerlabs.com>
2025-06-04 10:15:45 -0700

    Initial Commit

---
+++ Windows/WindowsApp.swift
@@ -0,0 +1,17 @@
+//
+//  WindowsApp.swift
+//  Windows
+//
+//  Created by Eric Groom on 6/4/25.
+//
+
+import SwiftUI
+
+@main
+struct WindowsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

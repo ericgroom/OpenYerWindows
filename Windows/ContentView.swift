73206b087c177022890bff363e714275f351c07f
Eric Groom <eric@gingerlabs.com>
2025-06-04 11:00:47 -0700

    display temperature for address

--- Windows/ContentView.swift
+++ Windows/ContentView.swift
@@ -6,16 +6,41 @@
 //
 
 import SwiftUI
+import WeatherKit
+import CoreLocation
 
 struct ContentView: View {
+    @State var manager = Manager()
+    @State var address = ""
+    @State var isFetching = false
+    @State var location: CLPlacemark? = nil
+    @State var weather: Weather? = nil
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        Form {
+            TextField("Address", text: $address)
+            Button("Fetch Location") {
+                guard !isFetching else { return }
+                isFetching = true
+                Task {
+                    defer { isFetching = false }
+                    self.location = try! await manager.getLocation(address: address)
+                }
+            }
+            Button("Fetch Weather") {
+                guard !isFetching else { return }
+                isFetching = true
+                Task {
+                    defer { isFetching = false }
+                    self.weather = try! await manager.getWeather(location: location!.location!)
+                }
+            }.disabled(location == nil)
+            if isFetching {
+                ProgressView()
+            }
+            Text("Weather: \(weather?.currentWeather.temperature.converted(to: .fahrenheit))")
+                .lineLimit(nil)
         }
-        .padding()
     }
 }
 

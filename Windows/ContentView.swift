a43f17dc1aa9d56bca4cf3d2be96b301d895c432
Eric Groom <eric@gingerlabs.com>
2025-06-04 11:21:07 -0700

    cache weather and location

--- Windows/ContentView.swift
+++ Windows/ContentView.swift
@@ -9,12 +9,60 @@ import SwiftUI
 import WeatherKit
 import CoreLocation
 
+@Observable
+class LocationManager {
+    let key = "user_location"
+    var location: CLPlacemark? {
+        didSet {
+            print("setting location to \(location)")
+            guard let location else { return } // TODO: clear
+            let data = try! NSKeyedArchiver.archivedData(withRootObject: location, requiringSecureCoding: true)
+            UserDefaults.standard.set(data, forKey: key)
+        }
+    }
+
+    init() {
+        if let data = UserDefaults.standard.data(forKey: key) {
+            let result = try? NSKeyedUnarchiver.unarchivedObject(ofClass: CLPlacemark.self, from: data)
+            self.location = result
+        }
+    }
+
+    func setAddress(_ address: String) async throws {
+        self.location = try await Manager().getLocation(address: address)
+    }
+}
+
+@Observable
+class WeatherManager {
+    let key = "user_weather"
+    var weather: Weather? {
+        didSet {
+            print("setting weather to \(weather)")
+            guard let weather else { return } // TODO: clear
+            let data = try! JSONEncoder().encode(weather)
+            UserDefaults.standard.set(data, forKey: key)
+        }
+    }
+
+    init() {
+        if let data = UserDefaults.standard.data(forKey: key) {
+            let result = try! JSONDecoder().decode(Weather.self, from: data)
+            self.weather = result
+        }
+    }
+
+    func updateWeather(_ location: CLLocation) async throws {
+        self.weather = try await Manager().getWeather(location: location)
+    }
+}
+
 struct ContentView: View {
     @State var manager = Manager()
     @State var address = ""
     @State var isFetching = false
-    @State var location: CLPlacemark? = nil
-    @State var weather: Weather? = nil
+    @State var locationManager = LocationManager()
+    @State var weatherManager = WeatherManager()
 
     var body: some View {
         Form {
@@ -24,7 +72,7 @@ struct ContentView: View {
                 isFetching = true
                 Task {
                     defer { isFetching = false }
-                    self.location = try! await manager.getLocation(address: address)
+                    try! await locationManager.setAddress(address)
                 }
             }
             Button("Fetch Weather") {
@@ -32,14 +80,16 @@ struct ContentView: View {
                 isFetching = true
                 Task {
                     defer { isFetching = false }
-                    self.weather = try! await manager.getWeather(location: location!.location!)
+                    try await weatherManager.updateWeather(locationManager.location!.location!)
                 }
-            }.disabled(location == nil)
+            }.disabled(locationManager.location == nil)
             if isFetching {
                 ProgressView()
             }
-            Text("Weather: \(weather?.currentWeather.temperature.converted(to: .fahrenheit))")
-                .lineLimit(nil)
+            Text("Weather: \(weatherManager.weather?.currentWeather.temperature.converted(to: .fahrenheit))")
+        }
+        .onAppear {
+            address = locationManager.location?.name ?? ""
         }
     }
 }
